bb3ca3b0fe1fc8e75548888e2f2c02b74c90d7bb
WxhKeepFighting <1003359353@qq.com>
2019-10-31 23:06:50 +0800

    重新提交代码

--- musicsystem/src/App.vue
+++ musicsystem/src/App.vue
@@ -3,19 +3,27 @@
     <div class="from_bos">
         <!-- form表单默认有个提交到当前页操作 -->
         <div class="form-group">
+            <center>
             <label for="name" class="col-sm-2 control-label">歌曲名称:</label><input type="text" id="name" placeholder="请输入歌曲名称" class="form-control" v-model="mymusic.musicname">
             <label for="author" class="col-sm-2 control-label">歌曲作者:</label><input type="text" placeholder="请输入歌曲作者" id="author" class="form-control" v-model="mymusic.musicauthor">
             <label for="type" class="col-sm-2 control-label">歌曲类型:</label><input type="text" placeholder="请输入歌曲类型" id="type" class="form-control" v-model="mymusic.musictype">
             <label for="date" class="col-sm-2 control-label">发行日期:</label><input type="date" id="date" class="form-control" v-model="mymusic.release_date">
+            </center>
         </div>
         <button @click="onSubmit()" class="btn btn-primary">提交</button>
     </div>
     <div>
-        <label for="select_name">查询歌曲名称</label><input type="text" placeholder="请输入歌名" id="select_name" class="form-control" v-model="mymusic.musicname">
-        <button @click="findByType()" class="btn btn-primary">查询</button>
+        <center>
+        <label for="select_name">查询歌曲类型</label><input type="text" placeholder="请输入歌曲类型" id="select_type" class="form-control" v-model="mymusic.musictype"><br>
+        <button @click="findByType()" class="btn btn-primary">查询</button><br><br>
+        <button @click="getmusic()" class="btn btn-primary">查询所有</button><br>
+        <label for="select_id">删除歌曲的id</label><input type="text" placeholder="请输入歌曲id" id="select_id" class="form-control" v-model="mymusic.id"><br>
+        <button @click="deleteById()" class="btn btn-primary" >删除</button>
+        </center>
     </div>
     <!-- <span>传入的json数组的长度为:{{musics.length}}</span>  -->
-    <table class="table table-striped">
+    <center>
+    <table class="table table-striped" >
       <thead>
           <th>歌曲名称</th>
           <th>歌曲作者</th>
@@ -23,22 +31,17 @@
           <th>发行日期</th>
       </thead>
       <tbody>
-          <!-- <tr>
-              <td>{{musics[0].musicname}}</td>
-              <td>{{musics[0].musicauthor}}</td>
-              <td>{{musics[0].musictype}}</td>
-              <td>{{musics[0].data}}</td>
-          </tr> -->
           <!-- v-bind 根据id变化来更新 -->
           <tr v-for="music in musicList" :key = "music" v-bind="music.id">
               <td>{{music.musicname}}</td>
               <td>{{music.musicauthor}}</td>
               <td>{{music.musictype}}</td>
-              <td>{{music.relase_date}}</td>
+              <td>{{music.release_date}}</td>
               <td></td>
           </tr>
       </tbody>
   </table>
+  </center>
   </div>
 </template>
 
@@ -48,7 +51,7 @@ import Axios from "axios";
 export default {
     //页面加载之前就执行的操作
   created(){
-       this.getmuseic();
+       this.getmusic();
   },
   data(){
       return{
@@ -64,7 +67,7 @@ export default {
       };
   },
   methods:{
-      getmuseic(){
+      getmusic(){
           Axios.get("http://localhost:8080/musics")
           .then(response => (this.musicList = response.data));//这里传入的是一个json数组
       },
@@ -83,14 +86,12 @@ export default {
           }));
       },
       findByType(){
-          Axios.get("http://localhost:8080/musics/type/"+this.mymusic.musicname)
-          .then(function(){
-            //   
-              response => 
-              (this.musicList = [],
-              this.musicList = response.data);
-            //   this.getmuseic();
-              });
+          Axios.get("http://localhost:8080/musics/type/"+this.mymusic.musictype)
+          .then(response => (this.musicList = response.data));
+      },
+      deleteById(){
+          Axios.delete("http://localhost:8080/musics/"+this.mymusic.id)
+          .then(this.getmusic)
       }
   }
 }
@@ -108,6 +109,8 @@ export default {
 
 #app table {
     margin-top: 50px;
+    width: 70%;
+    /* margin: auto; */
 }
 
 #app input{
